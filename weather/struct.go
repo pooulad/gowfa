ebdd440a6dc2a8700e8d979b8f2f74f133fa0e52
pooulad <poooooladi@gmail.com>
2023-11-08 22:32:48 +0330

    create Wheather Location struct

---
+++ weather/struct.go
@@ -0,0 +1,8 @@
+type Location struct {
+	Name      string `json:"name"`
+	Region    string `json:"region"`
+	Coutry    string `json:"country"`
+	Lat       string `json:"lat"`
+	Lon       string `json:"lon"`
+	LocalTime string `json:"localtime"`
+}
